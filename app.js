0c2ee4f4b567c2fcc6db1047a6a42d1b0b3acb7d
Rachel JTH <rachel98445111@gmail.com>
2022-08-31 23:58:24 +0800

    feat use body-parser pkg and get from info by post

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+// let every routes passing through the body-parser (included in "express")
+app.use(express.urlencoded({ extended: true }))
+
 // set template engines
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -12,6 +15,13 @@ app.set('view engine', 'handlebars')
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+// get form info
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.render('index')
+})
+
 
 // start the express server and listen for the connection
 app.listen(port, () => {
